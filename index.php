2ad3ca419549f1a2c5aff75265ea766307392e29
JoeryMartens <joerymartens@home.nl>
2015-02-03 13:56:05 +0100

    index.php
    Index file

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<?php
+include 'Webfont_declaration.php';
+?>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Page</title>
+  <meta name="description" content="HTML5">
+  <meta name="author" content="JoeryMartens">
+
+  <link rel="stylesheet" href="css/fonts.css">
+
+<style> 
+body {
+
+background-color: #000;
+
+}
+</style>
+
+
+</head>
+
+<body>
+ 
+<div class="content">
+	 <p>
+	 Hello
+	 </p> 
+</div>
+ 
+</body>
+</html>
+
